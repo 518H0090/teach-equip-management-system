b01eb85d23e389706b38daa59d1905bf7ba97990
518H0090 <nhoxhieuro5@gmail.com>
2024-06-29 10:34:17 +0700

    Apply Polly Retry Pattern to RestSharp

--- TeachEquipManagement/TeachEquipManagement.Utilities/Helper/RestSharpHelper.cs
+++ TeachEquipManagement/TeachEquipManagement.Utilities/Helper/RestSharpHelper.cs
@@ -1,4 +1,6 @@
-﻿using RestSharp;
+﻿using Polly;
+using Polly.Retry;
+using RestSharp;
 using System.Net;
 
 namespace TeachEquipManagement.Utilities.Helper
@@ -6,22 +8,38 @@ namespace TeachEquipManagement.Utilities.Helper
     public sealed class RestSharpHelper
     {
         private readonly RestClient _restClient;
+        private readonly AsyncRetryPolicy _policy;
+        private readonly int _maxRetryCount = 3;
+        private readonly TimeSpan _timeDelayForRetry = TimeSpan.FromSeconds(10);
 
         public RestSharpHelper(string url)
         {
             _restClient = new RestClient(url);
+            _policy = Policy
+           .Handle<Exception>()
+           .WaitAndRetryAsync(_maxRetryCount, retryAttempt => _timeDelayForRetry);
         }
 
-        public TEntity? Execute<TEntity>(RestRequest request) where TEntity : class, new()
+        public async Task<TEntity?> Execute<TEntity>(RestRequest request) where TEntity : class, new()
         {
-            var response = _restClient.Execute<TEntity>(request);
+            var response = await _policy.ExecuteAsync(async () =>
+            {
+                var response = await _restClient.ExecuteAsync<TEntity>(request);
+                return response;
+            });
+
             return response.Data;
         }
 
-        public HttpStatusCode Execute(RestRequest request)
+        public async Task<HttpStatusCode> Execute(RestRequest request)
         {
-            var respone = _restClient.Execute(request);
-            return respone.StatusCode;
+            var responseStatus = await _policy.ExecuteAsync(async () =>
+            {
+                var respone = await _restClient.ExecuteAsync(request);
+                return respone.StatusCode;
+            });
+
+            return responseStatus;
         }
 
     }
