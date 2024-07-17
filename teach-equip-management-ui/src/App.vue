800525667362f380a8c8abd6a8af0530218bcfff
518H0090 <nhoxhieuro5@gmail.com>
2024-07-17 22:10:15 +0700

    static simple sidebar

--- teach-equip-management-ui/src/App.vue
+++ teach-equip-management-ui/src/App.vue
@@ -47,10 +47,12 @@ button {
   main {
     flex: 1 1 0;
     padding: 2rem;
+  }
+}
 
-    @media (max-width: 768px) {
-      padding-left: 6rem;
-    }
+@media (max-width: 768px) {
+  .app main {
+    padding-left: 6rem;
   }
 }
 
--- teach-equip-management-ui/src/components/SideBar.vue
+++ teach-equip-management-ui/src/components/SideBar.vue
@@ -1,12 +1,31 @@
+<script setup>
+</script>
+
 <template>
     <aside>
         SideBar
     </aside>
 </template>
 
-<script setup>
-</script>
-
 <style lang="scss" scoped>
+
+aside {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--dark);
+    color: var(--light);
+    width: var(--sidebar-width);
+    overflow: hidden;
+    height: 100vh;
+    padding: 1rem;
+    transition: .2s ease-out;
+}
+
+@media (max-width: 768px) {
+    aside {
+        position: fixed;
+        z-index: 24;
+    }
+  }
 
 </style>
