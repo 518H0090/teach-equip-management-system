90a663afe5ea65add8fa17ba8decd9a90e4be29d
hieuhuynhtrantrung <trunghieuit1204@gmail.com>
2024-07-18 17:46:42 +0700

    seperate main to content

---
+++ teach-equip-management-ui/src/components/MainCard.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from "./Navbar.vue";
+</script>
+
+<template>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
--- teach-equip-management-ui/src/views/HomeView.vue
+++ teach-equip-management-ui/src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <script setup>
-
+import Navbar from "@/components/Navbar.vue";
+import MainCard from "@/components/MainCard.vue";
 </script>
 
 <template>
-  <main class="home-page">
-    <h1>Home</h1>
-    <p>This is home page</p>
-    <img src="https://trunghieu1204.sharepoint.com/:i:/s/FamilyTree/EQ_iu-huGVxDua8NcXLOkyUBn8z3Z1czavxDt0vZCVyARg" alt="">
-    <img src="https://trunghieu1204.sharepoint.com/sites/FamilyTree/Avatars/landscape-countryside-cartoon-scene-background-green-field-with-dirt-path-and-mountain-background-generative-ai-photo.jpg" alt="">
-  </main>
+  <div class="content">
+    <Navbar />
+    <MainCard>
+      <h1>Home</h1>
+      <p>This is home pagaaaaeaaaa</p>
+      <img
+        src="https://trunghieu1204.sharepoint.com/:i:/s/FamilyTree/EQ_iu-huGVxDua8NcXLOkyUBn8z3Z1czavxDt0vZCVyARg"
+        alt=""
+      />
+      <img
+        src="https://trunghieu1204.sharepoint.com/sites/FamilyTree/Avatars/landscape-countryside-cartoon-scene-background-green-field-with-dirt-path-and-mountain-background-generative-ai-photo.jpg"
+        alt=""
+      />
+    </MainCard>
+  </div>
 </template>
